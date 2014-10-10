db00ac70e69756be16407b3dd6cb9fbecfff4077
Juan M <jm@guide42.com>
2014-10-09 21:42:11 -0300

    First version

---
+++ traba.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Guide42\Traba;
+
+class Router
+{
+    protected $root;
+    protected $traverser;
+    protected $matcher;
+    protected $routes;
+
+    public function __construct($root,
+        callable $traverser=null,
+        callable $matcher=null
+    ) {
+        if ($traverser === null) {
+            $traverser = __NAMESPACE__ . '\\traverser';
+        }
+
+        if ($matcher === null) {
+            $matcher = __NAMESPACE__ . '\\matcher';
+        }
+
+        $this->root = $root;
+        $this->traverser = $traverser;
+        $this->matcher = $matcher;
+        $this->routes = [];
+    }
+
+    public function addRoute($handler, $context, $name) {
+        $this->routes[] = [$handler, $context, $name];
+    }
+
+    public function match(array $segments)
+    {
+        $node = call_user_func($this->traverser, $this->root, $segments);
+        $route = call_user_func($this->matcher, $this->routes,
+            $node['context'], $node['name']);
+
+        if ($route === null) {
+            throw new \RuntimeException('Route not found');
+        }
+
+        return [$route, $node['context']];
+    }
+
+    public function matchRequest($request)
+    {
+        if (method_exists($request, 'getUri')) {
+            $uri = (string) $request->getUri();
+        } else {
+            throw new \InvalidArgumentException(
+                'Could not find URI in the request'
+            );
+        }
+
+        return $this->match(array_filter(explode('/', $uri)));
+    }
+}
+
+function traverser($root, array $segments)
+{
+    foreach($segments as $i => $segment) {
+        if (!is_array($root) &&
+            !$root instanceof \ArrayAccess ||
+            !isset($root[$segment])
+        ) {
+            return array(
+                'context'   => $root,
+                'name'      => $segment,
+                'traversed' => array_slice($segments, 0, $i),
+                'after'     => array_slice($segments, $i + 1),
+            );
+        }
+
+        $root = $root[$segment];
+    }
+
+    return array(
+        'context'   => $root,
+        'name'      => '',
+        'traversed' => $segments,
+        'after'     => [],
+    );
+}
+
+function matcher($routes, $context, $name)
+{
+    foreach ($routes as $route) {
+        if ($route[2] === $name &&
+            ($route[1] === null ||
+            $context instanceof $route[1])
+        ) {
+            return $route[0];
+        }
+    }
+
+    return null;
+}
---
+++ traba_test.php
@@ -0,0 +1,130 @@
+<?php
+
+use Guide42\Traba\Router;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException        \RuntimeException
+     * @expectedExceptionMessage Route not found
+     */
+    public function testMatchRouteNotFound()
+    {
+        $router = new Router(array());
+        $router->addRoute('view_home', null, '');
+        $router->match(array('user', '123'));
+    }
+
+    public function testMatch()
+    {
+        $root = array(
+            'u' => new UserLocator(),
+        );
+
+        $router = new Router($root);
+        $router->addRoute('view_user', 'User', '');
+        $router->addRoute('view_user', 'User', 'edit');
+
+        list($route, $context) = $router->match(
+            array_filter(explode('/', '/u/123'))
+        );
+
+        $this->assertInstanceOf('User', $context);
+        $this->assertEquals('John Doe', $context->name);
+        $this->assertEquals('view_user', $route);
+    }
+
+    public function testMatchRequest()
+    {
+        $request = new Request('GET', 'http://localhost/user/123/edit');
+
+        $root = array(
+            'http:' => array(
+                'localhost' => array(
+                    'user' => new UserLocator(),
+                ),
+            ),
+        );
+
+        $router = new Router($root);
+        $router->addRoute('edit_user', 'User', 'edit');
+
+        list($route, $context) = $router->matchRequest($request);
+
+        $this->assertInstanceOf('User', $context);
+        $this->assertEquals('John Doe', $context->name);
+        $this->assertEquals('edit_user', $route);
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionMessage Could not find URI in the request
+     */
+    public function testMatchWithInvalidRequest()
+    {
+        $router = new Router(array());
+        $router->matchRequest(array());
+    }
+
+    public function testMatchWithoutContext()
+    {
+        $router = new Router(array());
+        $router->addRoute('view_home', null, '');
+        $router->addRoute('view_news', null, 'news');
+
+        list($rhome, $_) = $router->matchRequest(new Request('GET', '/'));
+        list($rnews, $_) = $router->matchRequest(new Request('GET', '/news'));
+
+        $this->assertEquals('view_home', $rhome);
+        $this->assertEquals('view_news', $rnews);
+    }
+}
+
+### FIXTURES ##################################################################
+
+class Request {
+    public $method;
+    public $uri;
+    public function __construct($method, $uri) {
+        $this->method = $method;
+        $this->uri = $uri;
+    }
+
+    public function getMethod() {
+        return $this->method;
+    }
+
+    public function getUri() {
+        return $this->uri;
+    }
+}
+
+class User {
+    public $name;
+    public $age;
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+}
+
+class UserLocator implements \ArrayAccess {
+    public $users;
+    public function __construct() {
+        $this->users = array(
+            123 => new User('John Doe', 42),
+        );
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->users[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return $this->users[$offset];
+    }
+
+    public function offsetSet($offset, $value) {}
+
+    public function offsetUnset($offset) {}
+}
